b201cfa736580df350f2d0189887ea9c993fed27
YvonneFang <yvonnef@andrew.cmu.edu>
2023-06-03 15:47:34 -0400

    user class

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import pkg from "@slack/bolt";
 import dotenv from "dotenv";
-import { messages } from "./messages.js";
+import { user_group } from "./messages.js";
 const { App } = pkg;
 
 dotenv.config();
@@ -23,124 +23,87 @@ const time3 = Math.floor((currentTime + 6 * 60 * 1000) / 1000);
 const time4 = Math.floor((currentTime + 8 * 60 * 1000) / 1000);
 const time5 = Math.floor((currentTime + 10 * 60 * 1000) / 1000);
 
-var times_skipped = 0;
-
-//add alt text for text
-// Test user 1 Vera
-await app.client.chat.scheduleMessage({
-  token: process.env.SLACK_BOT_TOKEN,
-  channel: process.env.SLACK_CHANNEL1,
-  text: "",
-  blocks: messages.morningmsg,
-  post_at: time1,
-});
-
-await app.client.chat.scheduleMessage({
-  token: process.env.SLACK_BOT_TOKEN,
-  channel: process.env.SLACK_CHANNEL1,
-  text: "",
-  blocks: messages.twominstretch,
-  post_at: time2,
-});
-
-await app.client.chat.scheduleMessage({
-  token: process.env.SLACK_BOT_TOKEN,
-  channel: process.env.SLACK_CHANNEL1,
-  text: "",
-  blocks: messages.twominstretch,
-  post_at: time3,
-});
-
-await app.client.chat.scheduleMessage({
-  token: process.env.SLACK_BOT_TOKEN,
-  channel: process.env.SLACK_CHANNEL1,
-  text: "",
-  blocks: messages.twominstretch,
-  post_at: time4,
-});
-
-await app.client.chat.scheduleMessage({
-  token: process.env.SLACK_BOT_TOKEN,
-  channel: process.env.SLACK_CHANNEL1,
-  text: "",
-  blocks: messages.eod,
-  post_at: time5,
-});
-
-// Test user 2 Zoya
-// await app.client.chat.scheduleMessage({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   channel: process.env.SLACK_CHANNEL1,
-//   text: "",
-//   blocks: messages.morningmsg,
-//   post_at: time1,
-// });
-
-// await app.client.chat.scheduleMessage({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   channel: process.env.SLACK_CHANNEL1,
-//   text: "",
-//   blocks: messages.twominstretch,
-//   post_at: time2,
-// });
-
-// await app.client.chat.scheduleMessage({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   channel: process.env.SLACK_CHANNEL1,
-//   text: "",
-//   blocks: messages.twominstretch,
-//   post_at: time3,
-// });
-
-// await app.client.chat.scheduleMessage({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   channel: process.env.SLACK_CHANNEL1,
-//   text: "",
-//   blocks: messages.twominstretch,
-//   post_at: time4,
-// });
-
-// await app.client.chat.scheduleMessage({
-//   token: process.env.SLACK_BOT_TOKEN,
-//   channel: process.env.SLACK_CHANNEL1,
-//   text: "",
-//   blocks: messages.eod,
-//   post_at: time5,
-// });
-
+//define some functions
 //conditional based on action_id
 //another input is channel, probably need an object to keep track
-async function respond_msg(id) {
+async function respond_msg(user, id) {
   app.client.chat.postMessage({
     token: process.env.SLACK_BOT_TOKEN,
-    channel: process.env.SLACK_CHANNEL1,
+    channel: user.channel,
     text: "",
     blocks:
       id === "button_complete"
         ? messages.complete
         : id === "button_snooze"
         ? messages.snooze
-        : id === "button_skip"
+        : id === "button_skip" && user.time_skipped == 1
         ? messages.skip1
+        : id === "button_skip" && user.time_skipped >= 2
+        ? messages.skip2
         : "unknown",
   });
 }
 
-//Basic messaging logic
-app.action("button_complete", async ({ action, ack, say }) => {
-  await ack();
-  await respond_msg("button_complete");
-});
+//add alt text for text
+//TODO: change welcome to morning after the first day
+for (user in user_group) {
+  await app.client.chat.scheduleMessage({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: user.channel,
+    text: "",
+    blocks: user.gen_welcome(),
+    post_at: time1,
+  });
 
-app.action("button_snooze", async ({ action, ack, say }) => {
-  await ack();
-  await respond_msg("button_snooze");
-});
+  await app.client.chat.scheduleMessage({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: user.channel,
+    text: "",
+    blocks: user.gen_stretch(),
+    post_at: time2,
+  });
 
-//Add +1 to time skipped here
-app.action("button_skip", async ({ action, ack, say }) => {
-  await ack();
-  await respond_msg("button_skip");
-});
+  await app.client.chat.scheduleMessage({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: user.channel,
+    text: "",
+    blocks: user.gen_stretch(),
+    post_at: time3,
+  });
+
+  await app.client.chat.scheduleMessage({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: user.channel,
+    text: "",
+    blocks: user.gen_stretch(),
+    post_at: time4,
+  });
+
+  await app.client.chat.scheduleMessage({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: user.channel,
+    text: "",
+    blocks: user.gen_eod(),
+    post_at: time5,
+  });
+
+  //Basic messaging logic
+  app.action("button_complete", async ({ action, ack }) => {
+    await ack();
+    await respond_msg(user, "button_complete");
+  });
+
+  app.action("button_snooze", async ({ action, ack }) => {
+    await ack();
+    await respond_msg(user, "button_snooze");
+  });
+
+  //Add +1 to time skipped here
+  app.action("button_skip", async ({ action, ack }) => {
+    await ack();
+    user.time_skipped += 1;
+    await respond_msg(user, "button_skip");
+  });
+}
 
 //Bot can automatically join public channels, but we need to manually invite it to a private channel
--- messages.js
+++ messages.js
@@ -1,355 +1,402 @@
-const image_vera = {};
-
-const welcomemsg = [
-  {
-    type: "divider",
-  },
-  {
-    type: "image",
-    title: {
-      type: "plain_text",
-      text: "I Need a Marg",
-      emoji: true,
-    },
-    image_url:
-      "https://assets3.thrillist.com/v1/image/1682388/size/tl-horizontal_main.jpg",
-    alt_text: "marg",
-  },
-  {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: ":robot_face: Welcome to Meeku, your neck-stretching companion! :tada::muscle:",
-      emoji: true,
-    },
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        type: "plain_text",
-        text: "This week, we're all about neck stretches. Get ready to pamper your neck and experience the joy of a relaxed, flexible spine. Let's make neck stretches the star of your fitness routine! :star2::mechanical_arm:",
-        emoji: true,
-      },
-    ],
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        type: "plain_text",
-        text: "Get ready to unleash the power of a happy, stretchy neck. Welcome to Meeku! :rocket::hugging_face:",
-        emoji: true,
-      },
-    ],
-  },
-  {
-    type: "divider",
-  },
-];
-
-const twominstretch = [
-  {
-    type: "image",
-    title: {
-      type: "plain_text",
-      text: "I Need a Marg",
-      emoji: true,
-    },
-    image_url:
-      "https://assets3.thrillist.com/v1/image/1682388/size/tl-horizontal_main.jpg",
-    alt_text: "marg",
-  },
-  {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: "2 min stretch message!",
-      emoji: true,
-    },
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        type: "plain_text",
-        text: "Give your neck the care it deserves with our rejuvenating 2-minute stretch session!",
-        emoji: true,
-      },
-    ],
-  },
-  {
-    type: "actions",
-    elements: [
-      {
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: "Work out now",
-        },
-        style: "primary",
-        value: "click_me_123",
-        action_id: "button_complete",
-      },
-      {
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: "Snooze",
-        },
-        value: "click_me_123",
-        action_id: "button_snooze",
-      },
-      {
-        type: "button",
-        text: {
-          type: "plain_text",
-          emoji: true,
-          text: "Skip",
-        },
-        style: "danger",
-        value: "click_me_123",
-        action_id: "button_skip",
-      },
-    ],
-  },
-];
-
-const skip1 = [
-  {
-    type: "divider",
-  },
-  {
-    type: "divider",
-  },
-  {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: "*Skip the neck stretch?* \n\n Your neck is giving you the bombastic side-eye. Show it some love!",
-    },
-    accessory: {
-      type: "image",
-      image_url:
-        "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-      alt_text: "cute cat",
-    },
-  },
-];
-
-const skip2 = [
-  {
-    type: "image",
-    title: {
-      type: "plain_text",
-      text: "Skipped two sessions",
-      emoji: true,
-    },
-    image_url:
-      "https://assets3.thrillist.com/v1/image/1682388/size/tl-horizontal_main.jpg",
-    alt_text: "marg",
-  },
-  {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: "Uh-oh! We’ve got a case of the neck neglector here! What’s the deal? ",
-      emoji: true,
-    },
-  },
-  {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: "Why did you skip?",
-    },
-    accessory: {
-      type: "static_select",
-      placeholder: {
-        type: "plain_text",
-        text: "Select a reason",
-        emoji: true,
-      },
-      options: [
-        {
-          text: {
-            type: "plain_text",
-            text: "I’m busy as a bee",
-            emoji: true,
-          },
-          value: "value-0",
-        },
-        {
-          text: {
-            type: "plain_text",
-            text: "Got late to work today",
-            emoji: true,
-          },
-          value: "value-1",
-        },
-        {
-          text: {
-            type: "plain_text",
-            text: "Other",
-            emoji: true,
-          },
-          value: "value-2",
-        },
-      ],
-      action_id: "static_select-action",
-    },
-  },
-];
-
-const eod = [
-  {
-    type: "divider",
-  },
-  {
-    type: "image",
-    title: {
-      type: "plain_text",
-      text: "I Need a Marg",
-      emoji: true,
-    },
-    image_url:
-      "https://assets3.thrillist.com/v1/image/1682388/size/tl-horizontal_main.jpg",
-    alt_text: "marg",
-  },
-  {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: ":robot_face: You rocked it, superstar! Meeku is beaming with pride! :tada::muscle:",
-      emoji: true,
-    },
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        type: "plain_text",
-        text: "Your accomplishments today are ------- absolutely incredible!\n Take a moment to revel in success :star2::fire:",
-        emoji: true,
-      },
-    ],
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        type: "plain_text",
-        text: "Rest up, recharge, and get ready to conquer tomorrow with the same determination.:crescent_moon::rocket:",
-        emoji: true,
-      },
-    ],
-  },
-  {
-    type: "divider",
-  },
-];
-
-const snooze = [
-  {
-    type: "divider",
-  },
-  {
-    type: "divider",
-  },
-  {
-    type: "section",
-    text: {
-      type: "mrkdwn",
-      text: "*I never pictured you to be a snoozer!* \n\n A few moments of stretching can work wonders for your neck, helping you feel refreshed and ready to tackle the day. So, let's give those neck muscles a wake-up call and start the day on the right note in 10 min!",
-    },
-    accessory: {
-      type: "image",
-      image_url:
-        "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-      alt_text: "cute cat",
-    },
-  },
-];
-
-const morningmsg = [
-  {
-    type: "divider",
-  },
-  {
-    type: "image",
-    title: {
-      type: "plain_text",
-      text: "I Need a Marg",
-      emoji: true,
-    },
-    image_url:
-      "https://assets3.thrillist.com/v1/image/1682388/size/tl-horizontal_main.jpg",
-    alt_text: "marg",
-  },
-  {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: ":robot_face: Good morning, superstar! Meeku is here to turbocharge your day! :sun_with_face:",
-      emoji: true,
-    },
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        type: "plain_text",
-        text: "Get ready to unleash your inner awesomeness and make today absolutely epic! :boom::rocket: Take a deep breath, put on your game face, and let's conquer the world together with stretches! You've got this! :smile::dizzy:",
-        emoji: true,
-      },
-    ],
-  },
-  {
-    type: "divider",
-  },
-];
-
-const complete = [
-  {
-    type: "image",
-    title: {
-      type: "plain_text",
-      text: "Skipped two sessions",
-      emoji: true,
-    },
-    image_url:
-      "https://assets3.thrillist.com/v1/image/1682388/size/tl-horizontal_main.jpg",
-    alt_text: "marg",
-  },
-  {
-    type: "header",
-    text: {
-      type: "plain_text",
-      text: "Whoop! Whoop! Look at you, neck exercise champion! You conquered the workout like a boss.",
-      emoji: true,
-    },
-  },
-  {
-    type: "context",
-    elements: [
-      {
-        type: "plain_text",
-        text: "Victory dance or a gentle neck stretch to celebrate? It’s your call. Keep up the fantastic work!",
-        emoji: true,
-      },
-    ],
-  },
-];
-
-export const messages = {
-  welcomemsg,
-  twominstretch,
-  skip1,
-  skip2,
-  eod,
-  snooze,
-  complete,
-  morningmsg,
+const vera = {
+  welcome:
+    "https://drive.google.com/file/d/12bYzm8ilYkNoltwJFNzJGs6fV0zc_0dz/view?usp=drive_link",
+  morning:
+    "https://drive.google.com/file/d/1oyCAkHZuQbW9rYlYK2rcMkB7ZDs7_61n/view?usp=drive_link",
+  stretch:
+    "https://drive.google.com/file/d/1zhRxkRExvI1ZjWp_RnzcfJRsWXuTizkE/view?usp=drive_link",
+  snooze:
+    "https://drive.google.com/file/d/18kkjlW7JZLnqAvNtsnhA3-G9znSfCRZh/view?usp=drive_link",
+  skip1:
+    "https://drive.google.com/file/d/1sNt7t_XAJjTsqXxLECj-ZCnMoSoMdUXg/view?usp=drive_link",
+  skip2:
+    "https://drive.google.com/file/d/1wuzBkpPOIgRM1EBDynqqS7ncMPmQG7cB/view?usp=drive_link",
+  eod: "https://drive.google.com/file/d/1AozOr5JAErfXudYCp7tm5w0Fq2RMXJ7X/view?usp=drive_link",
+  complete:
+    "https://drive.google.com/file/d/1CWDfZFjw7d0Fl-M2NWU_HfZKLZ4ECTUG/view?usp=drive_link",
 };
+
+const zoya = {
+  welcome:
+    "https://drive.google.com/file/d/15y0cCXnBaDxfQP-WsTzP0u7uJgc3POQK/view?usp=drive_link",
+  morning:
+    "https://drive.google.com/file/d/1CO_KJ7Dge26NXj12MnOB8Up05ZInK1mS/view?usp=drive_link",
+  stretch:
+    "https://drive.google.com/file/d/1dmWJ0RojqcXqrEUtqieT2as6wXJXv0VS/view?usp=drive_link",
+  snooze:
+    "https://drive.google.com/file/d/17ggDygAxaZILzYRmTLF1RL2weJ90jq-S/view?usp=drive_link",
+  skip1:
+    "https://drive.google.com/file/d/1th0U31qsNnHOGqCIAHNL0p0vXoXGbljV/view?usp=drive_link",
+  skip2:
+    "https://drive.google.com/file/d/19KX4aTt9mMsTVdbP_yhWzaoPqWtRBiAH/view?usp=drive_link",
+  eod: "https://drive.google.com/file/d/1IZl5706fWjt9KOdK7P9mNj0N76Mx0HrH/view?usp=drive_link",
+  complete:
+    "https://drive.google.com/file/d/1ZQeNV6EuNXnPpDP4pLmwAXEsrH9t4Z-3/view?usp=drive_link",
+};
+
+//initialize users
+//move this class code to messages.js
+class User {
+  constructor(name, channel, imglist) {
+    this.name = name;
+    this.channel = channel;
+    this.imglist = imglist;
+    this.time_skipped = 0;
+  }
+
+  gen_welcome() {
+    return [
+      {
+        type: "divider",
+      },
+      {
+        type: "image",
+        title: {
+          type: "plain_text",
+          text: "",
+          emoji: true,
+        },
+        image_url: this.imglist.welcome,
+        alt_text: "avatar",
+      },
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: ":robot_face: Welcome to Meeku, your neck-stretching companion! :tada::muscle:",
+          emoji: true,
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "plain_text",
+            text: "This week, we're all about neck stretches. Get ready to pamper your neck and experience the joy of a relaxed, flexible spine. Let's make neck stretches the star of your fitness routine! :star2::mechanical_arm:",
+            emoji: true,
+          },
+        ],
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "plain_text",
+            text: "Get ready to unleash the power of a happy, stretchy neck. Welcome to Meeku! :rocket::hugging_face:",
+            emoji: true,
+          },
+        ],
+      },
+      {
+        type: "divider",
+      },
+    ];
+  }
+
+  gen_stretch() {
+    return [
+      {
+        type: "image",
+        title: {
+          type: "plain_text",
+          text: "",
+          emoji: true,
+        },
+        image_url: this.imglist.stretch,
+        alt_text: "avatar",
+      },
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "2 min stretch message!",
+          emoji: true,
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "plain_text",
+            text: "Give your neck the care it deserves with our rejuvenating 2-minute stretch session!",
+            emoji: true,
+          },
+        ],
+      },
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "Work out now",
+            },
+            style: "primary",
+            value: "click_me_123",
+            action_id: "button_complete",
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "Snooze",
+            },
+            value: "click_me_123",
+            action_id: "button_snooze",
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              emoji: true,
+              text: "Skip",
+            },
+            style: "danger",
+            value: "click_me_123",
+            action_id: "button_skip",
+          },
+        ],
+      },
+    ];
+  }
+
+  gen_skip1() {
+    return [
+      {
+        type: "divider",
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*Skip the neck stretch?* \n\n Your neck is giving you the bombastic side-eye. Show it some love!",
+        },
+        accessory: {
+          type: "image",
+          image_url: this.imglist.skip1,
+          alt_text: "avatar",
+        },
+      },
+    ];
+  }
+
+  gen_skip2() {
+    [
+      {
+        type: "image",
+        title: {
+          type: "plain_text",
+          text: "Skipped two sessions",
+          emoji: true,
+        },
+        image_url: this.imglist.skip2,
+        alt_text: "avatar",
+      },
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "Uh-oh! We’ve got a case of the neck neglector here! What’s the deal? ",
+          emoji: true,
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Why did you skip?",
+        },
+        accessory: {
+          type: "static_select",
+          placeholder: {
+            type: "plain_text",
+            text: "Select a reason",
+            emoji: true,
+          },
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                text: "I’m busy as a bee",
+                emoji: true,
+              },
+              value: "value-0",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Got late to work today",
+                emoji: true,
+              },
+              value: "value-1",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "Other",
+                emoji: true,
+              },
+              value: "value-2",
+            },
+          ],
+          action_id: "static_select-action",
+        },
+      },
+    ];
+  }
+
+  gen_eod() {
+    return [
+      {
+        type: "divider",
+      },
+      {
+        type: "image",
+        title: {
+          type: "plain_text",
+          text: "",
+          emoji: true,
+        },
+        image_url: this.imglist.eod,
+        alt_text: "avatar",
+      },
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: ":robot_face: You rocked it, superstar! Meeku is beaming with pride! :tada::muscle:",
+          emoji: true,
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "plain_text",
+            text: "Your accomplishments today are ------- absolutely incredible!\n Take a moment to revel in success :star2::fire:",
+            emoji: true,
+          },
+        ],
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "plain_text",
+            text: "Rest up, recharge, and get ready to conquer tomorrow with the same determination.:crescent_moon::rocket:",
+            emoji: true,
+          },
+        ],
+      },
+      {
+        type: "divider",
+      },
+    ];
+  }
+
+  gen_snooze() {
+    return [
+      {
+        type: "divider",
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "*I never pictured you to be a snoozer!* \n\n A few moments of stretching can work wonders for your neck, helping you feel refreshed and ready to tackle the day. So, let's give those neck muscles a wake-up call and start the day on the right note in 10 min!",
+        },
+        accessory: {
+          type: "image",
+          image_url: this.imglist.snooze,
+          alt_text: "avatar",
+        },
+      },
+    ];
+  }
+
+  gen_morning() {
+    [
+      {
+        type: "divider",
+      },
+      {
+        type: "image",
+        title: {
+          type: "plain_text",
+          text: "",
+          emoji: true,
+        },
+        image_url: this.imglist.morning,
+        alt_text: "avatar",
+      },
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: ":robot_face: Good morning, superstar! Meeku is here to turbocharge your day! :sun_with_face:",
+          emoji: true,
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "plain_text",
+            text: "Get ready to unleash your inner awesomeness and make today absolutely epic! :boom::rocket: Take a deep breath, put on your game face, and let's conquer the world together with stretches! You've got this! :smile::dizzy:",
+            emoji: true,
+          },
+        ],
+      },
+      {
+        type: "divider",
+      },
+    ];
+  }
+
+  gen_complete() {
+    [
+      {
+        type: "image",
+        title: {
+          type: "plain_text",
+          text: "",
+          emoji: true,
+        },
+        image_url: this.imglist.complete,
+        alt_text: "avatar",
+      },
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "Whoop! Whoop! Look at you, neck exercise champion! You conquered the workout like a boss.",
+          emoji: true,
+        },
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "plain_text",
+            text: "Victory dance or a gentle neck stretch to celebrate? It’s your call. Keep up the fantastic work!",
+            emoji: true,
+          },
+        ],
+      },
+    ];
+  }
+}
+
+const user_vera = new User("Vera", "testuser1", vera);
+const user_zoya = new User("Zoya", "testuser2", zoya);
+
+export const user_group = [user_vera, user_zoya];
